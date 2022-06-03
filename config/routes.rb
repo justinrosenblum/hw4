1c1d6f8dc5cd783e6d6aaced7fe7954e9b586121
justinrosenblum <Justin.Rosenblum@kellogg.northwestern.edu>
2022-06-03 00:03:12 +0000

    finished hw 4 (for real)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # get "/", :controller => "places", :action => "index"
+  get "/", :controller => "places", :action => "index"
   get "/login", :controller => "sessions", :action => "new"
   get "/logout", :controller => "sessions", :action => "destroy"
   resources "places"
